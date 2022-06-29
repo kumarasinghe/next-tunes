b1d24027aee1fb10cc293c1ae3597b9bb78b7d68
Naveen Kumarasinghe <dndkumarasinghe@gmail.com>
2022-06-30 01:54:21 +0530

    empty message

--- src/containers/home/home.tsx
+++ src/containers/home/home.tsx
@@ -9,6 +9,12 @@ export default function Home() {
     <div>
       <main>
         <div className={style.resultsContainer}>
+          {!searchResults.length && (
+            <div className={style.emptySearch}>
+              <img className={style.emptyImage} src="/empty_results.png" />
+              <div>There's nothing here! Keep searching.</div>
+            </div>
+          )}
           {searchResults.map((track, idx) => {
             return <Card key={idx} className={style.card} {...track} />;
           })}
